973631d4ceb11a949c37833055ce7b878b9c44e4
GeethaC91 <geethabhupathi1209@gmail.com>
2022-02-25 01:13:14 -0500

    fresh change1

--- src/CircleArea.java
+++ src/CircleArea.java
@@ -3,7 +3,7 @@ public class CircleArea{
     public static void main(String[] args)
     {
         Scanner s=new Scanner(System.in);
-        System.out.println("Enter radius");
+        System.out.println("Please Enter the radius");
         int r=s.nextInt();
         double ar=area(r);
         System.out.println("the area of circle-->" + ar);
