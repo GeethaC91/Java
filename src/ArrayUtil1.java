c214c3aa23067d6ad518c605c8ea83a671527789
GeethaC91 <geethabhupathi1209@gmail.com>
2022-03-01 13:04:01 -0500

    latest

--- src/ArrayUtil1.java
+++ src/ArrayUtil1.java
@@ -7,6 +7,10 @@ public class ArrayUtil1 {
         System.out.println("The given int array is"+" "+Arrays.toString(a));
         int index=indexOf(a,b);
         System.out.println("The index of"+" "+b+" "+"is"+" "+index);
+        int int1=intAt(a,b);
+        System.out.println("The int at index"+" "+b+" "+"is"+" "+int1);
+        int len=length(a);
+        System.out.println("The length of array"+" "+"is"+" "+len);
         boolean b1=contains(a,b);
         System.out.println("The status is "+" "+b1);
     }
@@ -20,6 +24,23 @@ public class ArrayUtil1 {
             }
         }
         return -1;
+    }
+    public static int intAt(int[] a,int b)
+    {
+        if(b<a.length && b>=0)
+        return a[b];
+        else
+           // System.out.println("Please recheck");
+        return -1;
+    }
+    public static int length(int[] a)
+    {
+        int count=0;
+        for(int i:a)
+        {
+            count++;
+        }
+        return count;
     }
     public static boolean contains(int[] a,int b)
     {
