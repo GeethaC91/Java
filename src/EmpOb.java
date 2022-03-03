b15d69e5fd722da62cbf2b0eb6aeaff7e0d6b780
GeethaC91 <geethabhupathi1209@gmail.com>
2022-03-02 19:46:55 -0500

    latest

---
+++ src/EmpOb.java
@@ -0,0 +1,8 @@
+public class EmpOb {
+    public static void main(String[] args) {
+        Employee e = new Employee();
+        e.emp_id=12345;
+        e.name="Geetha";
+        e.dept="IT";
+    }
+}
--- src/Employee.java
+++ src/Employee.java
@@ -6,4 +6,14 @@ public class Employee {
     String dept;
     Date hiredate;
     double hourlyrate;
+    public void getEmpDetails()
+    {
+        StringBuffer sb=new StringBuffer();
+        sb.append("Employee Name="+name
+                +"\t"+"Employee id="+emp_id
+                +"\t"+"Department="+dept
+                +"\t"+"Hire Date="+hiredate
+                +"\t"+"Hourly Rate="+hourlyrate);
+        System.out.println(sb);
+    }
 }
